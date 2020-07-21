f33d94030000f12076c549d130221f72d81f1685
llb1997 <1443901216@qq.com>
2020-07-21 17:45:44 +0800

    修改测试文件内容

--- 111.Minimum_Depth_of_Binary_Tree/test_code111.py
+++ 111.Minimum_Depth_of_Binary_Tree/test_code111.py
@@ -1,12 +1,10 @@
 from code111 import Solution
-
-root1 = Solution().genTree([3,9,20,None,None,15,7])
-
-a=Solution().minDepth(root1)
-
-
-
 def test1():
+    root1 = Solution().genTree([3,9,20,None,None,15,7])
+
+    a=Solution().minDepth(root1)
+
+
     assert 2 == a
 
 if __name__ == '__main__': 
